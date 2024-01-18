40fbabb185923cfcd2264eed15848ac1bf79f064
farrizabalaga <farrizabalaga@alkemy-x.com>
2024-01-17 19:02:26 -0700

    Add some extra logging to figure out error

--- services/shotgrid_common/utils.py
+++ services/shotgrid_common/utils.py
@@ -29,6 +29,8 @@ def _sg_to_ay_dict(sg_entity: dict, project_code_field=None) -> dict:
     if not project_code_field:
         project_code_field = "code"
 
+    logging.debug("Transforming sg_entity '%s' to ayon dict.", sg_entity)
+    
     task_type = None
     if sg_entity["type"] == "Task":
         if not sg_entity["step"]:
