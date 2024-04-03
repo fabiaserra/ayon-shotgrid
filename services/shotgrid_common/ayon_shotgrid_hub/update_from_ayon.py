2a7e2942ddad38c375242c7770de9d6a3c83975c
Fabià Serra Arrizabalaga <fabia.serra92@gmail.com>
2024-04-03 19:46:27 +0200

    Add type to tag creation

--- services/shotgrid_common/ayon_shotgrid_hub/update_from_ayon.py
+++ services/shotgrid_common/ayon_shotgrid_hub/update_from_ayon.py
@@ -166,18 +166,18 @@ def update_sg_entity_from_ayon_event(
                 sg_tags = get_sg_tags(sg_session)
                 for tag_name in tags_event_list:
                     if tag_name.lower() in sg_tags:
-                        new_attribs["tags"].append(
-                            {"name": tag_name, "id": sg_tags[tag_name]}
-                        )
+                        tag_id = sg_tags[tag_name]
                     else:
                         logging.info(
                             f"Tag '{tag_name}' not found in ShotGrid, "
                             "creating a new one."
                         )
                         new_tag = sg_session.create("Tag", {'name': tag_name})
-                        new_attribs["tags"].append(
-                            {"name": tag_name, "id": new_tag["id"]}
-                        )
+                        tag_id = new_tag["id"]
+                    
+                    new_attribs["tags"].append(
+                        {"name": tag_name, "id": tag_id, "type": "Tag"}
+                    )
 
             else:
                 logging.warning("Unknown event type, skipping update of custom attribs.")
